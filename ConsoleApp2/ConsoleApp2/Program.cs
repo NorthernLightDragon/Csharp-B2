96b3e746e4d7a51f0964abfbe69a44544c51a7c1
NorthernLightDragon <42276346+NorthernLightDragon@users.noreply.github.com>
2020-01-21 08:53:37 +0100

    Add files via upload

---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Examination of water status");
+            Console.WriteLine("temperature: ");
+
+            Double a = Convert.ToDouble(Console.ReadLine());
+
+            if (a > 0)
+            {
+                if (a >= 100) Console.WriteLine("Steam");
+                else Console.WriteLine("Water");
+            }
+            else Console.WriteLine("Ice");
+
+            Console.ReadLine();
+        }
+    }
+}
